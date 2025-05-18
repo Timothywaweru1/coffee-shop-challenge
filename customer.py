125c79c6f0cdf568549c44282c66c3dffaeb1e4a
Timothy <timothy.waweru1@student.moringaschool.com>
2025-05-18 22:12:34 +0300

    finished customer

---
+++ customer.py
@@ -0,0 +1,17 @@
+class Customer:
+    def __init__(self,name):
+        if len(name) <= 15:
+            return self._name
+        else:
+            raise TypeError("Name must strictly contain 15 characters and not more!")
+
+    @property
+    def name(self):
+        return self.name
+    
+    @name.setter
+    def name(self,value):
+        if len(value) <= 15:
+            raise TypeError("Name must contain only 15 characters!")
+        else:
+            return self.value
